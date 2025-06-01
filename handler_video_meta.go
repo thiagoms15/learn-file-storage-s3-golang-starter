ac2d98876b3443fce50efde785b40a7f971d6574
Thiago Marques <thiago.siqueira@alticeusa.com>
2025-06-01 15:31:39 +0100

    Revert "Using security bucket"
    This reverts commit a64f397fc9cbe0cc1330aac86eb1f483e7b146e2.

--- handler_video_meta.go
+++ handler_video_meta.go
@@ -95,13 +95,7 @@ func (cfg *apiConfig) handlerVideoGet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	signedVideo, err := cfg.dbVideoToSignedVideo(video)
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Failed to generate presigned URL", err)
-		return
-	}
-
-	respondWithJSON(w, http.StatusOK, signedVideo)
+	respondWithJSON(w, http.StatusOK, video)
 }
 
 func (cfg *apiConfig) handlerVideosRetrieve(w http.ResponseWriter, r *http.Request) {
@@ -122,17 +116,5 @@ func (cfg *apiConfig) handlerVideosRetrieve(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
-	// Generate presigned URLs for each video
-	var signedVideos []database.Video
-	for _, video := range videos {
-		signedVideo, err := cfg.dbVideoToSignedVideo(video)
-		if err != nil {
-			respondWithError(w, http.StatusInternalServerError, "Failed to generate presigned URLs", err)
-			return
-		}
-		signedVideos = append(signedVideos, signedVideo)
-	}
-
-	respondWithJSON(w, http.StatusOK, signedVideos)
+	respondWithJSON(w, http.StatusOK, videos)
 }
-
